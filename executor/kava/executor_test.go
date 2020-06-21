7388e0662400cb98483c1b3ef91210bbd855519a
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-06-21 21:40:11 +0100

    improve checking in kava executor test

--- executor/kava/executor_test.go
+++ executor/kava/executor_test.go
@@ -5,17 +5,19 @@ package kava
 import (
 	"math/big"
 	"testing"
+	"time"
 
 	sdk "github.com/kava-labs/cosmos-sdk/types"
 	"github.com/kava-labs/go-sdk/client"
 	app "github.com/kava-labs/go-sdk/kava"
 	"github.com/stretchr/testify/require"
 
+	"github.com/binance-chain/bep3-deputy/store"
 	"github.com/binance-chain/bep3-deputy/util"
 )
 
 func TestSendAmount(t *testing.T) {
-	// TODO this test requires kvd to be running locally, with a funded deputy account.
+	// Note: This test requires kvd to be running locally, with a funded deputy account.
 
 	kavaConfig := sdk.GetConfig()
 	app.SetBech32AddressPrefixes(kavaConfig)
@@ -38,7 +40,26 @@ func TestSendAmount(t *testing.T) {
 	}
 	exe := NewExecutor(client.LocalNetwork, &config)
 
-	_, err = exe.SendAmount(config.ColdWalletAddr.String(), big.NewInt(100_000_000))
+	amountToSend := big.NewInt(100_000_000)
+	previousBalance, err := exe.GetBalance(config.ColdWalletAddr.String())
+
+	txHash, err := exe.SendAmount(config.ColdWalletAddr.String(), amountToSend)
 	require.NoError(t, err)
-	// TODO check coins have moved
+
+	waitUntilTxInBlock(exe, txHash, 10*time.Second)
+
+	// check coins have moved
+	balance, err := exe.GetBalance(config.ColdWalletAddr.String())
+	require.NoError(t, err)
+	require.Equal(t, balance, previousBalance.Add(previousBalance, amountToSend))
+}
+
+func waitUntilTxInBlock(executor *Executor, txHash string, timeout time.Duration) {
+	var status store.TxStatus
+	endTime := time.Now().Add(timeout)
+
+	for !(status == store.TxSentStatusSuccess || time.Now().After(endTime)) {
+		time.Sleep(500 * time.Millisecond)
+		status = executor.GetSentTxStatus(txHash)
+	}
 }
